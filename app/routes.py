f1acad539cfc47eb1652a826c41fc73be6f85f5e
frivolta <rivoltafilippo@gmail.com>
2020-09-30 15:02:30 +0200

    adding form

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,14 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-  return "Hello world"
+  user = {'username': 'filippo'}
+  posts = [
+    {
+      'author': {"username": "Filippo Rivolta"},
+      'body': 'Lorem ipsum dolor sit amet'
+    }
+  ]
+  return render_template('index.html', title='Home', user=user, posts=posts)
